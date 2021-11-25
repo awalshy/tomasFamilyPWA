7b97b2d203c24ee25f1de49692ca866773964879
Arthur Walsh <walsh.arthur@outlook.com>
2021-11-25 14:55:08 +0100

    merge nextjs app to reactjs app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux'
+import store from 'redux/store'
+import { createTheme, ThemeOptions, ThemeProvider } from '@material-ui/core';
+import ModalController from 'components/app/ModalController';
+import { BrowserRouter, Route } from 'react-router-dom'
+import firebaseConfig from 'config/firebase-config.json'
+import firebase from 'firebase'
+import Home from './routes/Home';
+import SignIn from './routes/SignIn';
+import Error404 from './routes/404';
+
+const themeOptions: ThemeOptions = {
+  palette: {
+    primary: {
+      main: '#133c6d'
+    },
+  }
+}
 
 function App() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    firebase.firestore().enablePersistence()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <ThemeProvider theme={createTheme(themeOptions)}>
+        <BrowserRouter>
+          <Route path="/" element={<Home />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="*" element={<Error404 />} />
+        </BrowserRouter>
+        <ModalController />
+      </ThemeProvider>
+    </Provider>
+  )
 }
 
 export default App;
