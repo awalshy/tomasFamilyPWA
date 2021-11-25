fcf9744bd3f60e25c7a8f9d4d95b7b6f314158e0
Arthur Walsh <walsh.arthur@outlook.com>
2021-11-25 21:54:43 +0100

    Skeleton + load images for family

--- src/routes/Gallery.tsx
+++ src/routes/Gallery.tsx
@@ -1,9 +1,59 @@
 import PageLayout from '../components/structure/PageLayout'
+import { useState } from 'react'
+import { useAppSelector } from 'src/redux/hooks'
+import { selectUserFamilyId } from 'src/redux/selectors'
+import { useEffect } from 'react'
+import API from 'src/firebase/api'
+import { Fab, ImageList, ImageListItem, useMediaQuery, useTheme } from '@material-ui/core'
+import { Add } from '@material-ui/icons'
+import { Skeleton } from '@material-ui/lab'
 
 const Gallery = () => {
+  const theme = useTheme()
+
+  const [urls, setUrls] = useState<string[]>([])
+  // const [loading, setLoading] = useState(false)
+  
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'))
+  const familyId = useAppSelector(selectUserFamilyId)
+
+  useEffect(() => {
+    const api = new API()
+    if (!familyId) return
+    api.gallery.loadPictures(familyId).then(res => {
+      setUrls(res)
+    })
+  }, [familyId, urls])
+
   return (
     <PageLayout title="Gallery | TOMAS">
-      <div>Gallery</div>
+      <div>
+        {urls.length > 0 && <ImageList cols={3}>
+          {urls.map(item => (
+            <ImageListItem>
+              <img src={item} alt="" />
+            </ImageListItem>
+          ))}
+        </ImageList>}
+        {urls.length === 0 && <ImageList cols={3}>
+          <ImageListItem>
+            <Skeleton variant="rect" width="20vh" height="20vh" />
+          </ImageListItem>
+          <ImageListItem>
+            <Skeleton variant="rect" width="20vh" height="20vh" />
+          </ImageListItem>
+          <ImageListItem>
+            <Skeleton variant="rect" width="20vh" height="20vh" />
+          </ImageListItem>
+          <ImageListItem>
+            <Skeleton variant="rect" width="20vh" height="20vh" />
+          </ImageListItem>
+          </ImageList>}
+        <Fab color="primary" variant={isMobile ? "circular" : "extended"} style={{ position: 'absolute', bottom: '10vh', right: '5vw' }}>
+          <Add />
+          {!isMobile && 'Upload'}
+        </Fab>
+      </div>
     </PageLayout>
   )
 }
