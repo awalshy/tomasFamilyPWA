3f5decb9553f831e0b49ffe552d0afcc7f09fbc8
Arthur Walsh <walsh.arthur@outlook.com>
2021-11-27 18:35:17 +0100

    Peak and Menu on Contact Page (#32)

---
+++ src/modal/PeakMember.tsx
@@ -0,0 +1,20 @@
+import { CircularProgress, Typography } from "@material-ui/core"
+import { useAppSelector } from "src/redux/hooks"
+import { selectMemberById } from "src/redux/selectors"
+import { RootState } from "src/redux/store"
+
+interface IPeakMember {
+  id: string
+}
+
+function PeakMember({ id }: IPeakMember) {
+  const member = useAppSelector((state: RootState) => selectMemberById(state, id))
+
+  if (!member) return <CircularProgress variant="indeterminate" color="primary" />
+
+  return <div style={{ padding: 20 }}>
+    <Typography variant="h5">{`${member.firstName} ${member.lastName.toLocaleUpperCase()}`}</Typography>
+  </div>
+}
+
+export default PeakMember
